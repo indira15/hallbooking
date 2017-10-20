61f73416e53336026cc758d3c839d4c969c1d39c
indira15 <indirasuthar@gmail.com>
2017-10-20 22:46:41 +0530

    view

---
+++ app/src/main/java/com/example/hallbook/hallbooking/entity/Request.java
@@ -0,0 +1,8 @@
+package com.example.hallbook.hallbooking.entity;
+
+/**
+ * Created by ARUN SUTHAR on 17-10-2017.
+ */
+
+public class Request {
+}
---
+++ app/src/main/java/com/example/hallbook/hallbooking/fcm/OwnersAdapter.java
@@ -0,0 +1,8 @@
+package com.example.hallbook.hallbooking.fcm;
+
+/**
+ * Created by ARUN SUTHAR on 17-10-2017.
+ */
+
+public class OwnersAdapter {
+}
